43f53de9cf0adf13326250ba172c3e7f5c375c38
abcworld123 <rkaxhdals@gmail.com>
2026-05-25 17:52:29 +0900

    Initial commit

---
+++ src/main/kotlin/com/abcworld/yongdrive/YongDriveApplication.kt
@@ -0,0 +1,11 @@
+package com.abcworld.yongdrive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YongDriveApplication
+
+fun main(args: Array<String>) {
+    runApplication<YongDriveApplication>(*args)
+}
---
+++ src/test/kotlin/com/abcworld/yongdrive/YongDriveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abcworld.yongdrive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YongDriveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
